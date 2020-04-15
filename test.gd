27d4343810dd009d4e2e5a6b78ade9958d9d95a5
systemchalk <systemchalk@gmail.com>
2020-04-15 03:01:06 -0400

    Changed TransitVehicle to check cargo_limit in collect * Removed cargo_limit specification equal to the default in _process * Corrected typos from previous comments * Removed incorrect comment about counter being decreased by one not having any effect * Adjusted line lengths to match what was implied by editor
    COMMENT: We discussed the possibility of surplus goods being thrown away at a supply point but the existing checks seem to prevent a surplus

--- test.gd
+++ test.gd
@@ -39,7 +39,8 @@ class SupplyPoint:
 		
 	# Constructor for SupplyPoint. Default supply points start without stock and
 	# demand 50 units of toilet paper. Default constraints are [0, 100]
-	func _init(new_max_level := 100, new_level := 0, new_demand := 50, new_max_demand := 100, new_min_demand := 0):
+	func _init(new_max_level := 100, new_level := 0, new_demand := 50,
+		new_max_demand := 100, new_min_demand := 0):
 		max_stock_level = new_max_level
 		stock_level = new_level
 		demand_level = new_demand
@@ -51,11 +52,11 @@ class TransitVehicle:
 	var cargo_limit : int
 	var cargo : int
 
-	# Vehicle collects the given amount of toilet paper, limited by the ammount
-	# available at the given supply point, and reduces the supply point's stock
+	# Vehicle collects the given amount of toilet paper, limited by the amount
+	# available at the given supply point and its carrying capacity, and reduces
+	# the supply point's stock
 	func collect(amount : int, sp : SupplyPoint) -> void:
-		# The simulation already constrains by cargo limit, but it feels like
-		# it should happen in collect
+		amount = min(amount, cargo_limit)
 		cargo = min(amount, sp.stock_level)
 		sp.stock_level -= cargo
 
@@ -72,7 +73,7 @@ class TransitVehicle:
 		cargo_limit = new_limit
 		cargo = new_cargo
 
-# Simulation requires a manufacturer, a warehouse, and a consumer whcih are
+# Simulation requires a manufacturer, a warehouse, and a consumer which are
 # defined as supply points
 var manufacturer : SupplyPoint
 var warehouse : SupplyPoint
@@ -94,7 +95,7 @@ func _process(delta):
 	# Make sure at least 1 second has passed before we run the simulation
 	counter += delta
 	if counter > 1:
-		counter -= 1 # This no longer serves any purpose
+		counter -= 1
 		# Manufacturer and consumer produce/consume at their demand_level in
 		# each period
 		manufacturer.produce_stock(manufacturer.demand_level)
@@ -103,22 +104,27 @@ func _process(delta):
 		# If the consumer has less stock than what they want in the next period
 		# they will go out to get more
 		if home.stock_level < home.demand_level:
-			var customer_vehicle = TransitVehicle.new(10) # Vehicle gets same capacity as default?
-			# Vehicle checks to make sure it doesn't ask for more than its
-			# capacity or more than it can store, then collects and delivers
-			customer_vehicle.collect(min(customer_vehicle.cargo_limit, min(home.demand_level / 2, home.max_stock_level - home.stock_level)), warehouse)
+			var customer_vehicle = TransitVehicle.new()
+			# Vehicle checks for how much is needed then collects and delivers
+			customer_vehicle.collect(min(home.demand_level / 2, 
+				home.max_stock_level - home.stock_level), warehouse)
 			customer_vehicle.deliver(home)
 		
 		# If the warehouse has less stock than what they want in the next period
 		# they wend a truck to get more
 		if warehouse.stock_level < warehouse.demand_level:
-			var delivery_truck = TransitVehicle.new(100) # Trucks are biggers than cars
-			# Vehicle checks to make sure it doesn't ask for more than it can
-			# carry or more than the warehouse can store, then collects/delivers
-			delivery_truck.collect(min(delivery_truck.cargo_limit, min(warehouse.demand_level, warehouse.max_stock_level - warehouse.stock_level)), manufacturer)
+			# Trucks are biggers than cars
+			var delivery_truck = TransitVehicle.new(100)
+			# Vehicle checks for how much is needed then collects and delivers
+			delivery_truck.collect(min(warehouse.demand_level,
+				warehouse.max_stock_level - warehouse.stock_level), 
+				manufacturer)
 			delivery_truck.deliver(warehouse)
 
-	# Print the current stock and demand for each SupplyPoint for debugging
-	print("Home stock: ", home.stock_level, ", Home demand: ", home.demand_level)
-	print("Warehouse stock: ", warehouse.stock_level, ", Warehouse demand: ", warehouse.demand_level)
-	print("Manufacturer stock: ", manufacturer.stock_level, ", Manufacturer demand: ", manufacturer.demand_level)
+		# Print the current stock and demand for each SupplyPoint for debugging
+		print("Home stock: ", home.stock_level, 
+			", Home demand: ", home.demand_level)
+		print("Warehouse stock: ", warehouse.stock_level, 
+			", Warehouse demand: ", warehouse.demand_level)
+		print("Manufacturer stock: ", manufacturer.stock_level, 
+			", Manufacturer demand: ", manufacturer.demand_level)
