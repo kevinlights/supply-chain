96c2d9d8a14526b670384c7a6118f143970932ff
systemchalk <systemchalk@gmail.com>
2020-05-07 23:54:03 -0400

    Adjusted Game.gd to make the first supply point a producer and last supply point a consumer. * Created a newspaper template scene

--- gui/Game.gd
+++ gui/Game.gd
@@ -22,7 +22,6 @@ func _ready():
 	# Instantiate the manufacturer, warehouse and home
 	sp_list.push_back(supply_point_scene.instance())
 	sp_list.back().init("Manufacturer")
-	sp_list.back().set_is_producing(true)
 
 	sp_list.push_back(supply_point_scene.instance())
 	sp_list.back().init("Warehouse")
@@ -34,18 +33,21 @@ func _ready():
 
 	sp_list.push_back(supply_point_scene.instance())
 	sp_list.back().init("Home", 100, 0, 40)
-	sp_list.back().set_is_consuming(true)
 	# sp_list.back().set_demand_factor(0.5)
 
 	setup_downstreams(sp_list)
 	add_supply_points(sp_list)
 
-# Iterates over each supply point and connects it to the destination for its cargo
+# Iterates over each supply point and connects it to the destination for its cargo and sets first entry as producer and final entry as consumer
 func setup_downstreams(list : Array) -> void:
+	var last : int = list.size() - 1
+
 	for i in range(0, list.size()):
-		if i == list.size() -1:
+		if i == last:
 			break
 		list[i].set_downstream(list[i + 1])
+	list[0].set_is_producing(true)
+	list[last].set_is_consuming(true)
 
 # Adds supply points to game
 func add_supply_points(list: Array) -> void:
